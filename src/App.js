c72714c4b03255c61e6bfc82efc339b23c604920
Walter Pariona <wltrst97@gmail.com>
2020-10-27 00:44:05 -0500

    Added function to fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import useFetchJobs from './api/useFetchJobs';
+import { Container } from 'react-bootstrap';
 
 function App() {
-  return <h1>Hello Papijuancho!</h1>;
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+
+  const { jobs, loading, error } = useFetchJobs(params, page);
+
+  return (
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error. Try refreshing...</h1>}
+      <h1>{jobs.length}</h1>
+      {jobs.map((job) => (
+        <p>{job.title}</p>
+      ))}
+    </Container>
+  );
 }
 
 export default App;
