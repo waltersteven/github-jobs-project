1a46e4cab7e911748a2539fe5f99fcd48fdd2f13
Walter Pariona <wltrst97@gmail.com>
2020-10-29 00:01:16 -0500

    Added Job component to display information

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 import useFetchJobs from './api/useFetchJobs';
 import { Container } from 'react-bootstrap';
+import Job from './components/job/Job';
 
 function App() {
-  const [params, setParams] = useState({});
+  const [params, setParams] = useState({ search: 'code' });
   const [page, setPage] = useState(1);
 
   const { jobs, loading, error } = useFetchJobs(params, page);
 
   return (
-    <Container>
+    <Container className='my-4'>
+      <h1 className='mb-4'>Github Jobs</h1>
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error. Try refreshing...</h1>}
-      <h1>{jobs.length}</h1>
       {jobs.map((job) => (
-        <p>{job.title}</p>
+        <Job key={job.id} job={job}></Job>
       ))}
     </Container>
   );
