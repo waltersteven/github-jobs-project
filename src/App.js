4a6f28f6c2ba3be1b52797b3dbb4f48f15e6d3b3
Walter Pariona <wltrst97@gmail.com>
2020-10-29 23:22:47 -0500

    Added search form feature

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import useFetchJobs from './api/useFetchJobs';
 import { Container } from 'react-bootstrap';
 import Job from './components/job/Job';
 import JobsPagination from './components/job/JobsPagination';
+import SearchForm from './components/form/SearchForm';
 
 function App() {
   const [params, setParams] = useState({});
@@ -12,9 +13,20 @@ function App() {
 
   const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
 
+  function handleParamChange(e) {
+    const param = e.target.name;
+    const value = e.target.value;
+    setPage(1);
+    setParams((prevParams) => {
+      return { ...prevParams, [param]: value };
+    });
+    console.log('finished handleParamChange');
+  }
+
   return (
     <Container className='my-4'>
       <h1 className='mb-4'>Github Jobs</h1>
+      <SearchForm params={params} onParamChange={handleParamChange} />
       <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error. Try refreshing...</h1>}
--- src/api/useFetchJobs.js
+++ src/api/useFetchJobs.js
@@ -27,6 +27,7 @@ function reducer(state, action) {
 
 export default function useFetchJobs(params, page) {
   const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
+  console.log('useFetchJobs state >>>', state);
 
   useEffect(() => {
     const cancelToken1 = axios.CancelToken.source();
